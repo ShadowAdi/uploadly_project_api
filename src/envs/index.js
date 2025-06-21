03b036a2e513f68eb2350b36e38068ec2b056d90
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-21 11:49:52 +0530

    Added Loggers in git ignore

--- src/envs/index.js
+++ src/envs/index.js
@@ -4,6 +4,8 @@ export const configEnvs = () => {
   configDotenv();
 };
 
+configEnvs()
+
 export const PORT = process.env.PORT;
 export const MONGODB_URL = process.env.MONGODB_URL;
 export const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY;
