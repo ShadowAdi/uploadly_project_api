cdc0dcc777c48c1b084a9a7346117d15a55c3dcb
Aditya Shukla <Shadowshukla76@gmail.com>
2025-06-23 08:20:29 +0530

    Added Env variables

--- src/envs/index.js
+++ src/envs/index.js
@@ -11,3 +11,6 @@ export const MONGODB_URL = process.env.MONGODB_URL;
 export const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY;
 export const NSFW_API_USER=process.env.NSFW_API_USER
 export const NSFW_API_SECRET=process.env.NSFW_API_SECRET
+export const CLOUDINARY_API_SECRET=process.env.CLOUDINARY_API_SECRET;
+export const CLOUDINARY_API_KEY=process.env.CLOUDINARY_API_KEY;
+export const CLOUDINARY_CLOUD_NAME=process.env.CLOUDINARY_CLOUD_NAME;
